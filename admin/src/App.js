370af2d0078d02d02a3f76d87a853d3820e1aa32
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-15 12:11:28 -0300

    sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,7 @@
-import Ordenes from "./components/paginas/Ordenes"
-import Menu from "./components/paginas/Menu"
-import NuevoPlato from "./components/paginas/NuevoPlato"
+import Ordenes from "./components/views/Ordenes"
+import Menu from "./components/views/Menu"
+import NuevoPlato from "./components/views/NuevoPlato"
+import Sidebar from "./components/ui/Sidebar"
 
 
 import { Routes, Route } from "react-router-dom"
@@ -9,6 +10,7 @@ import { Routes, Route } from "react-router-dom"
 export default function App() {
   return (
     <>
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<Ordenes/>} />
         <Route path="/menu" element={<Menu/>} />
