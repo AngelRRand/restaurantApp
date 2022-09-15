91c02ca83ea6bba6c63ff494529e9545b1b7b3d6
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-15 19:28:07 -0300

    estructure

--- admin/src/components/ui/Platillo.js
+++ admin/src/components/ui/Platillo.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Platillo = () => {
+const Platillo = ({platillo}) => {
+
+    const {nombre, precio, categoria, imagen, description} = platillo
+
   return (
     <div className='w-full px-3 mb-4'>
-        <h1>hola</h1>
+        <div className='p-5 shadow-md bg-gray-800'>
+            <div className='lg:flex'>
+                <div className='lg:w-5/12 xl:w-3/12'>
+                    <img src={imagen} alt='plato'/>
+                </div>
+                <div className='lg:w-5/12'>
+                
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
