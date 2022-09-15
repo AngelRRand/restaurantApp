9598175291bde0911a76d7187bf5acd8ff244d80
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-15 13:36:50 -0300

    select

--- admin/src/components/views/NuevoPlato.js
+++ admin/src/components/views/NuevoPlato.js
@@ -8,10 +8,41 @@ const NuevoPlato = () => {
       <div className=' w-full max-w-3xl'>
           <form>
             <div className='mb-4'>
-              <label>Nombre</label>
+              <label className='block text-gray-700 text-sm font-blod mb-2' htmlFor='nombre'>Nombre</label>
               <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                id='nombre'
+                type='text'
+                placeholder='Pizza Napolitana'
               />
+            </div>
+
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-blod mb-2' htmlFor='precio'>Precio</label>
+              <input
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                id='precio'
+                type='number'
+                placeholder='$100'
+                min='0'
+              />
+            </div>
+
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-blod mb-2' htmlFor='categoria'>Categoria</label>
+              <select
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                id='categoria'
+                name='categoria'
+              >
+                <option value=''>-- Seleccione --</option>
+                <option value='Desayuno'> Desayuno </option>
+                <option value='Comida'> Comida </option>
+                <option value='Cena'> Cena </option>
+                <option value='Bebida'> Bebida </option>
+                <option value='Postre'> Postre </option>
+                <option value='Ensalada'> Ensalada </option>
+              </select>
             </div>
           </form>
       </div>
