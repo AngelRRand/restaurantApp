a9cdd3d93ef04dc7b420e1b0502acaa96d220a71
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-15 12:58:15 -0300

    sideBar

--- admin/src/components/ui/Sidebar.js
+++ admin/src/components/ui/Sidebar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="md:w-2/5 xl:w-1/5 bg-gray-900">
+      <div className='p-6'>
+        <p className='text-white uppercase text-3xl tracking-wide text-center font-bold'>
+          RestaurantApp
+        </p>
+        <p className='mt-3 text-gray-600'>Administra tu restaurante, explora y expande tu negocio</p>
+        <nav>
+          <NavLink activeClassName="" to='/'>Ordenes</NavLink>
+          <NavLink activeClassName="" to='/menu'>Menu</NavLink>
+        </nav>
+      </div>
+    </div>
   )
 }
 
