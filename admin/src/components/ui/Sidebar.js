077332d0ed1785aff4cf42df39a4861462e3e3de
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-15 19:00:11 -0300

    colorsFixed

--- admin/src/components/ui/Sidebar.js
+++ admin/src/components/ui/Sidebar.js
@@ -8,16 +8,16 @@ const Sidebar = () => {
   return (
     <div className="md:w-2/5 xl:w-1/5 bg-gray-900">
       <div className='p-6'>
-        <p className='text-white uppercase text-2xl tracking-wide text-center font-bold'>
-          RestaurantApp
+        <p className='uppercase text-4xl tracking-wide text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-pink-400'>
+          Nanaka Ramen Comer
         </p>
         <p className='mt-3 text-gray-600'>Administra tu restaurante, explora y expande tu negocio</p>
         <nav>
           <NavLink className={
-            `${urlActual === '/' ? 'text-yellow-400' : 'text-gray-400'} p-1 block hover:text-yellow-200`}
+            `${urlActual === '/' ? 'text-red-400' : 'text-gray-400'} p-1 block hover:text-red-200`}
             to="/">Ordenes</NavLink>
           <NavLink className={
-            `${urlActual === '/menu' ? 'text-yellow-400' : 'text-gray-400'} p-1 block hover:text-yellow-200`}
+            `${urlActual === '/menu' ? 'text-red-400' : 'text-gray-400'} p-1 block hover:text-red-200`}
             to="/menu">Menu</NavLink>
         </nav>
       </div>
