216140afeb5757fbfe68d67774902ace7b4206c5
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-14 22:31:00 -0300

    routerInstall

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,15 @@
+import Ordenes from "./components/paginas/Ordenes"
 
+
+import { Routes, Route } from "react-router-dom"
 
 
 export default function App() {
   return (
-    <h1 className="text-2xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Routes>
+        <Route path="/" element={<Ordenes/>} />
+      </Routes>
+    </>
   )
 }
