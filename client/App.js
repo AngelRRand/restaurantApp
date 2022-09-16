1be03c9bd2d7ab0004c898dcd9e9e539cd7cc123
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-16 15:51:52 -0300

    importComponent

--- client/App.js
+++ client/App.js
@@ -1,15 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import DetallesPlato from './src/view/DetallesPlato.js';
+import FormularioPlato from './src/view/FormularioPlato.js';
+import Menu from './src/view/Menu.js';
+import NuevaOrden from './src/view/NuevaOrden.js';
+import ProgresoPedido from './src/view/ProgresoPedido.js';
+import ResumenPedido from './src/view/ResumenPedido.js';
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
+
       </Stack.Navigator>
     </NavigationContainer>
   );
