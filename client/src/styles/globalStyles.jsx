9092093127cc059263ae0a8d0a4abea4eea13c57
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-18 17:22:38 -0300

    desinstalar

---
+++ client/src/styles/globalStyles.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const stylesGlobal = StyleSheet.create({
+    container:{
+        
+    }
+});
+
+export default stylesGlobal
--- client/src/view/NuevaOrden.js
+++ client/src/view/NuevaOrden.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Text } from 'react-native'
+
 
 const NuevaOrden = () => {
   return (
-    <View>
-      <Text>NuevaOrden</Text>
-    </View>
+    <>
+    </>
   )
 }
 
