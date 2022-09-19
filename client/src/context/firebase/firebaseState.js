320214c5878884e9a9927592a72997d3c3d0eefb
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-19 13:27:18 -0300

    gitIgnore

--- client/src/context/firebase/firebaseState.js
+++ client/src/context/firebase/firebaseState.js
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 import firebase from "../../../firebase";
 import FirebaseReducer from "./firebaseReducer";
 import FirebaseContext from "./firebaseContext";
-
+import { OBTENER_PRODUCTOS } from "../../types";
 const FirebaseState = props =>{
 
     //console.log(firebase)
@@ -12,6 +12,12 @@ const FirebaseState = props =>{
 
     //useReducer con dispatch para ejecutar las funciones
     const [state, dispatch] = useReducer(FirebaseReducer, initialState)
+
+
+    //Trae los productos
+    const obtenerProductos = () =>{
+        
+    }
 
     return(
         <FirebaseContext.Provider
