4d4b8e20d1424a9b5a8a1950dfc77409095333f9
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-18 21:19:34 -0300

    structure

--- client/src/view/NuevaOrden.js
+++ client/src/view/NuevaOrden.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import stylesGlobal from '../styles/stylesGlobal.jsx';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -9,7 +9,12 @@ const NuevaOrden = () => {
       <LinearGradient
         colors={['#0f0f0f', '#1d1d1d', '#421b53']}
         style={stylesGlobal.container}>
-        <Text>Hola</Text>
+        <View>
+          <Button
+            title="Learn More"
+            color="#841584"
+          />
+        </View>
       </LinearGradient>
     </>
   )
