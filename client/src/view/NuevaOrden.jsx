18909a2c1f26603e0aae2ac628aef0621fab2d41
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-19 13:15:23 -0300

    stylesScreen

--- client/src/view/NuevaOrden.jsx
+++ client/src/view/NuevaOrden.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native'
+import { Button, Text, View, StyleSheet } from 'react-native'
 import stylesGlobal from '../styles/stylesGlobal.jsx';
 import { LinearGradient } from 'expo-linear-gradient';
+import {useNavigation} from '@react-navigation/native'
 
 const NuevaOrden = () => {
+
+  const navigation = useNavigation()
+
   return (
     <>
       <LinearGradient
         colors={['#0f0f0f', '#1d1d1d', '#421b53']}
         style={stylesGlobal.container}>
-        <View>
+        <View style={[stylesGlobal.contenido, styles.contenido]}>
           <Button
             title="Learn More"
             color="#841584"
+            onPress={()=> navigation.navigate('menu')}
           />
         </View>
       </LinearGradient>
@@ -20,4 +25,10 @@ const NuevaOrden = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  contenido:{
+    flexDirection:'column',
+    justifyContent:'center'
+  }
+})
 export default NuevaOrden
