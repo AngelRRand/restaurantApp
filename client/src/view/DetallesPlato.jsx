ac4974f2f904c84d050650f6dd4d8c547ac2f07d
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-20 20:28:54 -0300

    useContext

--- client/src/view/DetallesPlato.jsx
+++ client/src/view/DetallesPlato.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import PedidosbaseContext from '../context/pedidos/pedidosContext';
 
 const DetallesPlato = () => {
+
+  const {plato} = useContext(PedidosbaseContext)
+
   return (
     <View>
       <Text>Holi</Text>
