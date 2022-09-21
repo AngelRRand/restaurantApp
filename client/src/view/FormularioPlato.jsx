5f237810165c16b5785cabf5fc0454c2b77a06f3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-21 00:05:35 -0300

    estructure

--- client/src/view/FormularioPlato.jsx
+++ client/src/view/FormularioPlato.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, Image, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import {useNavigation} from '@react-navigation/native'
+import PedidosbaseContext from '../context/pedidos/pedidosContext';
 import stylesGlobal from '../styles/stylesGlobal.jsx';
 
 const FormularioPlato = () => {
   return (
-    <View>
-      <Text style={[stylesGlobal.titulo]}>FormularioPlato</Text>
-    </View>
+    <LinearGradient
+      colors={['#0f0f0f', '#3d1515']}
+      style={stylesGlobal.container}>
+        <View style={stylesGlobal.contenido}>
+            
+        </View>
+      </LinearGradient>
   )
 }
 
