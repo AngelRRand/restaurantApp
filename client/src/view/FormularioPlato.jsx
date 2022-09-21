9a434728ef8cde2b9be66c3f9836a2ca3e9b632b
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-21 16:35:34 -0300

    formPlato

--- client/src/view/FormularioPlato.jsx
+++ client/src/view/FormularioPlato.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 import PedidosbaseContext from '../context/pedidos/pedidosContext';
 import stylesGlobal from '../styles/stylesGlobal.jsx';
 
@@ -10,11 +10,29 @@ const FormularioPlato = () => {
     <LinearGradient
       colors={['#0f0f0f', '#3d1515']}
       style={stylesGlobal.container}>
-        <View style={stylesGlobal.contenido}>
-            
+      <View style={stylesGlobal.contenido}>
+        <View style={[stylesGlobal.separator, styles.containerText]}>
+          <Text style={stylesGlobal.titulo}>Pedido</Text>
         </View>
-      </LinearGradient>
+        <View style={[stylesGlobal.separator, styles.containerPlus]}>
+          
+        </View>
+      </View>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+  containerText: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerPlus: {
+    justifyContent:'space-around',
+    alignItems:'center',
+    flexDirection:'row',
+    marginVertical:30
+  }
+})
 
 export default FormularioPlato
