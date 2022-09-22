5a19b577217642dc35aafed07b3556c41d303fb2
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-22 12:39:02 -0300

    funcion

--- client/src/component/BotonResumen.jsx
+++ client/src/component/BotonResumen.jsx
@@ -4,13 +4,16 @@ import { useNavigation } from '@react-navigation/native'
 const BotonResumen = () => {
     const navigation = useNavigation()
     return (
-        <Pressable>
+        <Pressable style={styles.btn} onPress={()=>navigation.navigate('resumenPedido')}>
             <Text style={styles.text}>Tu pedido</Text>
         </Pressable>
     );
 }
 
 const styles = StyleSheet.create({
+    btn:{
+        marginHorizontal:40
+    },
     text: {
         color: "#fcdc75",
         textAlign: 'center',
