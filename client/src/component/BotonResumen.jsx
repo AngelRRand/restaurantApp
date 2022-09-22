6b6daf9f724f656b7a07220a4f121e727ba86286
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-22 12:42:32 -0300

    a

--- client/src/component/BotonResumen.jsx
+++ client/src/component/BotonResumen.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import {Pressable, Text, StyleSheet} from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import React, { useContext, useEffect } from 'react';
+import { Pressable, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import PedidosbaseContext from '../context/pedidos/pedidosContext';
+
+
 const BotonResumen = () => {
+
+    const { pedido } = useContext(PedidosbaseContext)
     const navigation = useNavigation()
+
+
     return (
-        <Pressable style={styles.btn} onPress={()=>navigation.navigate('resumenPedido')}>
-            <Text style={styles.text}>Tu pedido</Text>
-        </Pressable>
+        <>
+            {
+                pedido.lenght === 0 ?
+                    (null)
+                    :
+                    (
+                    <Pressable style={styles.btn} onPress={() => navigation.navigate('resumenPedido')}>
+                        <Text style={styles.text}>Tu pedido</Text>
+                    </Pressable>
+                    )
+            }
+        </>
+
     );
 }
 
 const styles = StyleSheet.create({
-    btn:{
-        marginHorizontal:40
+    btn: {
+        marginHorizontal: 40
     },
     text: {
         color: "#fcdc75",
         textAlign: 'center',
         fontSize: 20,
         letterSpacing: 3
-      }
+    }
 })
 
 export default BotonResumen;
