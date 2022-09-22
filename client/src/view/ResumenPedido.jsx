2e6e8d0c054809dd4fba64527d08cdd68fb7d8d1
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-21 22:04:12 -0300

    resumenPedidos

--- client/src/view/ResumenPedido.jsx
+++ client/src/view/ResumenPedido.jsx
@@ -1,15 +1,41 @@
 import React, { useContext, useState } from 'react';
-import { View, StyleSheet, Text, Image, Button, Alert } from 'react-native';
+import { View, StyleSheet, Text, FlatList } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 import PedidosbaseContext from '../context/pedidos/pedidosContext';
 import stylesGlobal from '../styles/stylesGlobal.jsx';
 
 const ResumenPedido = () => {
+
+  const { pedido } = useContext(PedidosbaseContext)
+
   return (
-    <View>
-      <Text>ResumenPedido</Text>
-    </View>
+    <LinearGradient
+      colors={['#0f0f0f', '#3d1515']}
+      style={stylesGlobal.container}>
+
+      <View style={stylesGlobal.contenido}>
+        <View style={[stylesGlobal.separator, styles.containerText]}>
+          <Text style={[stylesGlobal.titulo]}>Pedidos</Text>
+        </View>
+        <View style={[stylesGlobal.separator, styles.containerText]}>
+
+          <FlatList
+            data={pedido}
+            keyExtractor={item => item.id}
+            renderItem={({ item, index }) => {
+              return (
+
+                <Card item={item} index={index} mostrarHeading={mostrarHeading} seleccionarPlato={seleccionarPlato} />
+              )
+            }}
+          />
+
+        </View>
+
+      </View>
+
+    </LinearGradient>
   )
 }
 
